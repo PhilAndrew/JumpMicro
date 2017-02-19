5648eed810c5456b89f349c4e458c7d00f084151
Philip Andrew <phil@orsa-studio.com>
2017-02-19 13:51:42 +0800

    Using bootstrap 4 visual style

--- JMCloner/src/main/scala/jumpmicro/jmcloner/impl/actor/WebServer.scala
+++ JMCloner/src/main/scala/jumpmicro/jmcloner/impl/actor/WebServer.scala
@@ -19,6 +19,8 @@ object WebServer extends KorolevBlazeServer {
   val service = blazeService[Future, State, Any] from KorolevServiceConfig [Future, State, Any] (
     stateStorage = storage,
     head = 'head(
+      'meta('charset /= "utf-8"),
+      'meta('name /= "viewport", 'content /= "width=device-width, initial-scale=1, shrink-to-fit=no"),
       'link('rel /= "stylesheet", 'type /= "text/css", 'href /= "tether-1.3.3/dist/css/tether.css"),
       'link('rel /= "stylesheet", 'type /= "text/css", 'href /= "bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css"),
       'script('src /= "jquery/jquery-3.1.1.js"),
@@ -26,6 +28,85 @@ object WebServer extends KorolevBlazeServer {
       'script('src /= "bootstrap-4.0.0-alpha.6-dist/js/bootstrap.js")),
     render = {
       case state =>
+
+        /*
+          <div class="container">
+          <div class="header clearfix">
+            <nav>
+              <ul class="nav nav-pills float-right">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </nav>
+            <h3 class="text-muted">Project name</h3>
+          </div>
+         */
+        'body(
+          'div('class /= "container",
+            'div('class /= "header clearfix",
+            'nav(), 'h3('class /= "text-muted", "Project name")),
+
+            /*
+                      <div class="jumbotron">
+                        <h1 class="display-3">Jumbotron heading</h1>
+                        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+                      </div>
+             */
+            'div('class /= "jumbotron",
+              'h1('class /= "display-3", "Jumbotron heading"),
+              'p('class /= "lead", "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."),
+              'p('a('class /= "btn btn-lg btn-success", 'href /= "#", 'role /= "button", "Sign up today"))),
+
+            /*
+            <div class="row marketing">
+              <div class="col-lg-6">
+                <h4>Subheading</h4>
+                <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                <h4>Subheading</h4>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                <h4>Subheading</h4>
+                <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+              </div>
+
+              <div class="col-lg-6">
+                <h4>Subheading</h4>
+                <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                <h4>Subheading</h4>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                <h4>Subheading</h4>
+                <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+              </div>
+            </div>
+             */
+
+            'div('class /= "row marketing",
+              'div('class /= "col-lg-6",
+                'h4("Sub heading"),
+                'p("Hello")),
+              'div('class /= "col-lg-6",
+                'h4("Sub heading"),
+                'p("Hello")))
+          )
+        )
+
+
+/*          <footer class="footer">
+            <p>&copy; Company 2017</p>
+          </footer>*/
+
+/*
         'body(
           'div("Super TODO tracker"),
           'div(
@@ -84,7 +165,7 @@ object WebServer extends KorolevBlazeServer {
             ),
             'button("Add todo")
           )
-        )
+        )*/
     },
     serverRouter = {
       ServerRouter(
