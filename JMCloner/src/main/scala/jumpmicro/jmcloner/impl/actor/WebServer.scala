f23aca47995e939fbfbc05425c386b851fa764b1
Philip Andrew <phil@orsa-studio.com>
2017-02-19 12:39:31 +0800

    js and css files should be in the static directory

--- JMCloner/src/main/scala/jumpmicro/jmcloner/impl/actor/WebServer.scala
+++ JMCloner/src/main/scala/jumpmicro/jmcloner/impl/actor/WebServer.scala
@@ -1,16 +1,10 @@
 package jumpmicro.jmcloner.impl.actor
 
-//: ----------------------------------------------------------------------------------
-//: Copyright © 2017 Philip Andrew https://github.com/PhilAndrew  All Rights Reserved.
-//: Released under the MIT License, refer to the project website for licence information.
-//: ----------------------------------------------------------------------------------
-
 import korolev._
 import korolev.server._
 import korolev.blazeServer._
 
 import scala.concurrent.Future
-
 
 /**
   * @author Aleksey Fomkin <aleksey.fomkin@gmail.com>
@@ -19,38 +13,50 @@ object WebServer extends KorolevBlazeServer {
 
   import State.effects._
 
-  // Handler to input
+  val storage = StateStorage.default[Future, State](State())
   val inputId = elementId
 
-/*
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-*/
   val service = blazeService[Future, State, Any] from KorolevServiceConfig [Future, State, Any] (
+    stateStorage = storage,
     head = 'head(
-      'link('rel /= "stylesheet", 'href /= "tether-1.3.3/dist/css/tether.css"),
-      'link('rel /= "stylesheet", 'href /= "bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css"),
+      'link('rel /= "stylesheet", 'type /= "text/css", 'href /= "tether-1.3.3/dist/css/tether.css"),
+      'link('rel /= "stylesheet", 'type /= "text/css", 'href /= "bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css"),
       'script('src /= "jquery/jquery-3.1.1.js"),
       'script('src /= "tether-1.3.3/dist/js/tether.js"),
       'script('src /= "bootstrap-4.0.0-alpha.6-dist/js/bootstrap.js")),
-    serverRouter = ServerRouter.empty[Future, State],
-    stateStorage = StateStorage.default(State()),
     render = {
       case state =>
         'body(
           'div("Super TODO tracker"),
-          'div('style /= "height: 250px; overflow-y: scroll",
-            (state.todos zipWithIndex) map {
+          'div(
+            state.todos.keys map { name =>
+              'span(
+                event('click) {
+                  immediateTransition { case s =>
+                    s.copy(selectedTab = name)
+                  }
+                },
+                'style /= "margin-left: 10px",
+                if (name == state.selectedTab) 'strong(name)
+                else name
+              )
+            }
+          ),
+          'div('class /= "todos",
+            (state.todos(state.selectedTab) zipWithIndex) map {
               case (todo, i) =>
                 'div(
-                  'input(
-                    'type /= "checkbox",
-                    'checked when todo.done,
+                  'div(
+                    'class /= {
+                      if (!todo.done) "checkbox"
+                      else "checkbox checkbox__checked"
+                    },
                     // Generate transition when clicking checkboxes
                     event('click) {
-                      immediateTransition { case tState =>
-                        val updated = tState.todos.updated(i, tState.todos(i).copy(done = !todo.done))
-                        tState.copy(todos = updated)
+                      immediateTransition { case s =>
+                        val todos = s.todos(s.selectedTab)
+                        val updated = todos.updated(i, todos(i).copy(done = !todo.done))
+                        s.copy(todos = s.todos + (s.selectedTab -> updated))
                       }
                     }
                   ),
@@ -65,8 +71,8 @@ object WebServer extends KorolevBlazeServer {
               deferredTransition {
                 access.property[String](inputId, 'value) map { value =>
                   val todo = State.Todo(value, done = false)
-                  transition { case tState =>
-                    tState.copy(todos = tState.todos :+ todo)
+                  transition { case s =>
+                    s.copy(todos = s.todos + (s.selectedTab -> (s.todos(s.selectedTab) :+ todo)))
                   }
                 }
               }
@@ -79,15 +85,54 @@ object WebServer extends KorolevBlazeServer {
             'button("Add todo")
           )
         )
+    },
+    serverRouter = {
+      ServerRouter(
+        dynamic = (_, _) => Router(
+          fromState = {
+            case State(tab, _) =>
+              Root / tab.toLowerCase
+          },
+          toState = {
+            case (s, Root) =>
+              val u = s.copy(selectedTab = s.todos.keys.head)
+              Future.successful(u)
+            case (s, Root / name) =>
+              val key = s.todos.keys.find(_.toLowerCase == name)
+              Future.successful(key.fold(s)(k => s.copy(selectedTab = k)))
+          }
+        ),
+        static = (deviceId) => Router(
+          toState = {
+            case (_, Root) =>
+              storage.initial(deviceId)
+            case (_, Root / name) =>
+              storage.initial(deviceId) map { s =>
+                val key = s.todos.keys.find(_.toLowerCase == name)
+                key.fold(s)(k => s.copy(selectedTab = k))
+              }
+          }
+        )
+      )
     }
   )
 }
 
-case class State(todos: Vector[State.Todo] = (0 to 2).toVector map {
-  i => State.Todo(s"This is TODO #$i", done = false)
-})
+case class State(
+                  selectedTab: String = "Tab1",
+                  todos: Map[String, Vector[State.Todo]] = Map(
+                    "Tab1" -> State.Todo(5),
+                    "Tab2" -> State.Todo(7),
+                    "Tab3" -> State.Todo(2)
+                  )
+                )
 
 object State {
   val effects = Effects[Future, State, Any]
   case class Todo(text: String, done: Boolean)
+  object Todo {
+    def apply(n: Int): Vector[Todo] = (0 to n).toVector map {
+      i => Todo(s"This is TODO #$i", done = false)
+    }
+  }
 }
